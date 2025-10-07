8a32e0862048b6faf0c85185706362512218fd0a
YoussefAlaadin <youssefalaadin86@gmail.com>
2025-10-07 17:55:46 +0300

    structure

---
+++ Backend/src/main/java/com/spring/fullstacktodo/FullStackToDoApplication.java
@@ -0,0 +1,15 @@
+package com.spring.fullstacktodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class FullStackToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackToDoApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
---
+++ Backend/src/test/java/com/spring/fullstacktodo/FullStackToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.fullstacktodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
